a7571a4d79bea4beb123784d285384aef86fe47e
Alessandra Souza <alegalliard@MacBook-Pro-de-Alessandra.local>
2019-12-23 09:16:51 -0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { CounterComponent } from './counter/counter.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'star-wars-search', component: SearchComponent },
+  { path: 'counter', component: CounterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

aa8cefb681fb75b958e28152691be1322915c2f9
Alessandra Souza <alegalliard@MacBook-Pro-de-Alessandra.local>
2019-12-22 22:52:47 -0300

    switchMap, tap

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { startWith, map, mergeMap, filter, debounceTime, retry } from 'rxjs/operators';
+import { startWith, map, tap, mergeMap, filter, debounceTime, retry, switchMap } from 'rxjs/operators';
 
 // URL: `https://swapi.co/api/people/?search=${v}`
 
@@ -14,13 +14,13 @@ Objetivos:
 5. deve esperar o usuário parar de digitar por pelo menos meio segundo - OK (ver debounceTime) 
 6. deve tentar fazer a requisição pelo menos 5 vezes antes de dar erro - OK (ver retry)
 7. deve exibir uma mensagem de 'carregando...' toda vez q começar uma nova busca  - OK (ver startWith no pipe da conexão)
-8. um resultado de uma busca antiga nunca deve sobrepor uma nova
+8. um resultado de uma busca antiga nunca deve sobrepor uma nova - OK (ver switchMap X margeMap)
 
 Definições
 
 map: aplica uma dada função para cada valo emitido pelo Observable de origem e emite o valor como um Observable
 
-mergeMap: projeta cada valor de origem num Observable e é mergeado como Observable final
+mergeMap: projeta cada valor de origem num Observable e é mergeado como Observable final, não importa a ordem
 
 filter: filtra itens emitidos pelo Observable de origem emitindo somente aqueles q satisfazem a regra
 
@@ -28,6 +28,7 @@ debounceTime: emite um valor do Observable de origem apenas após o tempo defini
 
 retry: retorna um Observable q espelha o Observable de origem quando há erro/exception e tenta a chamada novamente quantas vezes forem definidas
 
+switchMap: projeta cada valor de origem num Observable e é mergeado como Observable final, mas só emite o mais recente
 */
 
 
@@ -45,13 +46,14 @@ export class SearchComponent {
     debounceTime(500),
     filter(val => val.length > 2),
     map(val => `https://swapi.co/api/people/?search=${val}`), 
-    mergeMap(url => this.http.get(url) 
+    switchMap(url => this.http.get(url) 
         .pipe(
           retry(5),
           map(results => results['results']),
           startWith({message: 'Carregando...'})
         )),
-    startWith({message: 'Não foi possível carregar a lista'})
+    startWith({message: 'Digite um nume para iniciar'}),
+    tap(console.log)
   );
 
   constructor(

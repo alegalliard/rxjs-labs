a8219025f41a73a6af3ab3d0ce464855869504f7
Alessandra Souza <alegalliard@MacBook-Pro-de-Alessandra.local>
2019-12-26 22:29:13 -0300

    counter - initial exercise

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { mapTo, tap } from 'rxjs/operators';
+
+/*
+Objetivos:
+1. não devemos mutar nenhum dado!
+2. deve separar as ações que manipulam o estado dos efeitos colaterais
+3. deve aumentar o contador quando clicar no botão add
+4. deve subtrair o contador quando clicar no botão sub
+5. deve resetar o contador quando clicar no botão reset
+6. deve iniciar o contador somando um a cada segundo quando clicar no botão start
+7. deve pausar o contador quando clicar no botão pause
+*/
+
+enum Action {
+  Start,
+  Pause,
+  Reset,
+  Add,
+  Subtract,
+}
+
+interface AppState {
+  count: number,
+}
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
 
-  constructor() { }
+  private initialState: AppState = {
+    count: 0,
+  };
 
-  ngOnInit() {
-  }
+  actionType = Action;
+  actions$ = new BehaviorSubject(Action.Reset);
 
+  state$: Observable<AppState> = this.actions$.pipe(
+    tap(a => console.log(Action[a])),
+    mapTo({...this.initialState}),
+  );
 }

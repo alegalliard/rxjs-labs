243682c23e65b7ad56ccfe3fe4df6960da20c10b
Alessandra Souza <alegalliard@MacBook-Pro-de-Alessandra.local>
2020-01-05 18:10:09 -0300

    complex-counter route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { CounterComponent } from './counter/counter.component';
+import { ComplexCounterComponent } from './complex-counter/complex-counter.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', 
@@ -20,6 +21,12 @@ const routes: Routes = [
     component: CounterComponent,
     data: {
       title: 'Counter'
+    } 
+  },
+  { path: 'complex-counter', 
+    component: ComplexCounterComponent,
+    data: {
+      title: 'Complex Counter'
     } 
   }
 ];
